112b31c710196187e6a92486e446a27ea4f6c9dc
Avery Miller <averymiller2060@gmail.com>
2024-04-25 13:19:39 -0400

    added placeholder image to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import './HomePage.css';
+import placeholder from '../assets/images/placeholder_image.png';
 
 export default function HomePage() {
   return (
     <>
+    <img className="placeholder" src={placeholder} alt="placeholder image" />
     </>
   );
 }
